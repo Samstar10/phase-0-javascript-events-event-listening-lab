39210fb2ee9f63d90ff2ac86bf8cbb49d29325da
Samuel Muli <samuelmuli95@gmail.com>
2023-09-17 10:27:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    let input = document.querySelector('#button')
+    input.addEventListener('click', () => {
+        alert('I have an event listener attached to me')
+    })
 }
+
+
